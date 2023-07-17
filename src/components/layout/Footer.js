c7b962771c5a46c8cce9e4e7e6c8220c0bef6227
yanibustos <yanibustos4596@gmail.com>
2023-07-17 16:18:20 -0300

    Estilos footer y main content

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -2,6 +2,6 @@ import React from 'react';
 
 export const Footer = () => {
   return (
-    <footer>Portafolio Yanina Bustos &copy; Master en React - 2023</footer>
+    <footer className='footer'>Portafolio Yanina Bustos &copy; Master en React - 2023</footer>
   )
 }
--- src/router/MisRutas.js
+++ src/router/MisRutas.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Routes,Route,BrowserRouter,NavLink} from 'react-router-dom';
+import { Routes, Route, BrowserRouter, NavLink } from 'react-router-dom';
 import { Inicio } from '../components/Inicio';
 import { Portafolio } from '../components/Portafolio';
 import { Servicios } from '../components/Servicios';
@@ -11,22 +11,22 @@ import { Footer } from '../components/layout/Footer';
 export const MisRutas = () => {
   return (
     <BrowserRouter>
-        {/*Header & Nav*/}
-        <HeaderNav/>
-        {/*Main Content*/}
-
+      {/*Header & Nav*/}
+      <HeaderNav />
+      {/*Main Content*/}
+      <section className='content'>
         <Routes>
-            <Route path='/' element={<Inicio/>}></Route>
-            <Route path='/inicio' element={<Inicio/>}></Route>
-            <Route path='/portafolio' element={<Portafolio/>}></Route>
-            <Route path='/servicios' element={<Servicios/>}></Route>
-            <Route path='/curriculum' element={<Curriculum/>}></Route>
-            <Route path='/contacto' element={<Contacto/>}></Route>
+          <Route path='/' element={<Inicio />}></Route>
+          <Route path='/inicio' element={<Inicio />}></Route>
+          <Route path='/portafolio' element={<Portafolio />}></Route>
+          <Route path='/servicios' element={<Servicios />}></Route>
+          <Route path='/curriculum' element={<Curriculum />}></Route>
+          <Route path='/contacto' element={<Contacto />}></Route>
         </Routes>
+      </section>
 
-        <hr/>
-        {/*Footer*/}
-        <Footer/>
+      {/*Footer*/}
+      <Footer />
     </BrowserRouter>
   )
 }
